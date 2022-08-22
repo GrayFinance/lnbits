87f5d57695939dca78740404516fb540fb47f333
アレフ <93302536+leffw@users.noreply.github.com>
2022-08-22 18:15:58 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name="lnbits", version="0.1.0", py_modules=["lnbits"], install_requires=["requests"])
+setup(name="lnbits", version="0.1.0", py_modules=["lnbits"], install_requires=["requests", "cachetools"])
